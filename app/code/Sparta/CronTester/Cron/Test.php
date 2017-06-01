06c5e880a94d65d8c5bd65c1322f94672171cbf4
Yuriy Denyshchenko <ydenyshchenk@magento.com>
2017-06-01 22:44:10 +0300

    Added sleep(90)

--- app/code/Sparta/CronTester/Cron/Test.php
+++ app/code/Sparta/CronTester/Cron/Test.php
@@ -11,7 +11,10 @@ class Test
      */
     public function execute()
     {
-        error_log('Sparta_CronTester: ' . date("Y-m-d H:i:s") . "\n", 3, 'var/log/Sparta_CronTester.log');
+        error_log('Sparta_CronTester: start ' . date("Y-m-d H:i:s") . "\n", 3, 'var/log/Sparta_CronTester.log');
+        error_log('Sparta_CronTester: sleep 90 seconds ' . date("Y-m-d H:i:s") . "\n", 3, 'var/log/Sparta_CronTester.log');
+        sleep(90);
+        error_log('Sparta_CronTester: end ' . date("Y-m-d H:i:s") . "\n", 3, 'var/log/Sparta_CronTester.log');
 
         return $this;
     }
